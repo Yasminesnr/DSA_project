071054c25aae2d3b380156f1f13010752398b1ac
NadaZanoun <131750565+NadaZanoun@users.noreply.github.com>
2023-12-22 17:54:58 +0100

    Update PrizeWinners.h

--- PrizeWinners.h
+++ PrizeWinners.h
@@ -13,18 +13,26 @@ public:
 	Year* lastYear; //tail of the linked list 
 
 	//Parameterized constructor
-	PrizeWinners(int);
+	PrizeWinners(int firstYear);
 
 	//a function to insert a new year to the list
 	void addYear();
 
 	//function to traverse the list to get to a specific year
-	void traverse(Year* ,int);
+	void traverse(Year* temp,int year);
 
-	//function to set the Prize Winners on a specific month
+	//function to set the 5 Prize Winners on a specific month
 	void set5PrizeWinners(int year, int month, Restaurant winners[]);
 
-	//function to retrieve the winners from any specefic month a return a pointer to an array of restaurants
+	//function to set a winner of one cuisine in one cuisine on any specefic month (i is the index of the cuisine)
+	//1: Algerian, 2: Chinese, 3: European, 4: Indian, 5: Syrian
+	void setOneCuisineWinner(int year, int mont, Restaurant restaurant, int i);
+
+	//function to retrieve the 5 winners from any specefic month a return a pointer to an array of restaurants
 	Restaurant* get5PrizeWinners(int year,int month);
+
+	//function to get a winner of one cuisine in one cuisine on any specefic month (i is the index of the cuisine)
+	//1: Algerian, 2: Chinese, 3: European, 4: Indian, 5: Syrian
+	Restaurant getOneCuisineWinner(int year, int month, int i);
 };
 
