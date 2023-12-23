e59d1cca7fdbae3d6824622453c640628104fddb
NadaZanoun <131750565+NadaZanoun@users.noreply.github.com>
2023-12-23 22:00:22 +0100

    Update binarySearchTree.hpp

--- binarySearchTree.hpp
+++ binarySearchTree.hpp
@@ -1,169 +1,170 @@
-#ifndef binarySearchTree_hpp
-#define binarySearchTree_hpp
+#ifndef BST_h
+#define BST_h
 
 #include <iostream>
 #include <algorithm>
 #include <string>
 #include <vector>
-#include "Restaurant.hpp"
+#include <iomanip>
+#include "Restaurant.h"
+#include "PrizeWinners.h"
 using namespace std;
+
+
 
 //define a class binary search tree
 
-class BinarySearchTree {
+class binarySearchTree {
     //define the data members and member functions as protcted because they will be used later for the avl
 protected:
-    
-    
     //vectors to store the Algerian Cities /wilayas
-        vector<int> AlgerianCities;
-        vector<int> AlgerianWilayas;
-    
-    
-    //define a struct TreeNode
-    struct TreeNode{
-        
-    protected:
-        Restaurant element;
-        TreeNode* left;
-        TreeNode* right;
-        int height;//to be used in the avl
-    public:
-        //constructor
-        TreeNode(const Restaurant &E,TreeNode* l,TreeNode* r):element(E),left(l),right(r){}
-        //move constructor
-        TreeNode(Restaurant &&theElement, TreeNode *lt, TreeNode *rt)
-                   : element{std::move(theElement)}, left{lt}, right{rt} {}
-        friend class BinarySearchTree;//so that we can use the treenode elements 
-    };
-    //end of the struct
-    
-    TreeNode* root;
-    
+    vector<int> AlgerianCities;
+    vector<int> AlgerianWilayas;
+
+    Restaurant* root;
+
+
     //the functions needed
-    
+
     //FindMin
-    TreeNode * findMinHelper( TreeNode *t ) const;
+    Restaurant* findMinHelper(Restaurant* t) const;
     //FindMax
-    TreeNode* findMaxHelper( TreeNode *t ) const;
+    Restaurant* findMaxHelper(Restaurant* t) const;
     //contains
-    bool containsHelper( const Restaurant & x, TreeNode *t ) const;
+    bool containsHelper(const Restaurant& x, Restaurant* t) const;
     //clone
-    TreeNode * clone( TreeNode *t ) const;
+    //Restaurant* clone(Restaurant* t) const;
     //makeEmpty
-    void makeEmptyHelper( TreeNode * & t );
-    
+    void makeEmptyHelper(Restaurant*& t);
+
     //printTree
-    void printTreeHelper( TreeNode *t, ostream & out ) const;
-    
+    void printTreeHelper(Restaurant* t, ostream& out) const;
+
+
     //insertHelper
-    void insertHelper( const Restaurant & x, TreeNode * & t );
+    void addRestaurant(const Restaurant& x,Restaurant*& t);
     //deleteHelper
-    void removeHelper( const Restaurant & x, TreeNode * & t );
+    void removeHelper(const Restaurant& x, Restaurant*& t);
     //searchHelper
-    TreeNode *findRestaurantByIDHelper(const string& id,TreeNode *t) const;
-    
+    Restaurant*& findRestaurantByIDHelper(const string& id, Restaurant* t) const;
+
+    Restaurant*& findRestaurantByNameHelper(const string& name, Restaurant* t) const;
+
     //a function to check if the entered city is a city
-    bool IsCity(const string &city) const;
+    bool IsCity(const string& city) const;
     //a function to check if the entered  district is a district
-    bool IsDistrict(const string &district) const;
+    bool IsDistrict(const string& district) const;
     //a function to check if the entered wilaya is a wilaya
-    bool IsWilaya(const string &wilaya) const;
-    
+    bool IsWilaya(const string& wilaya) const;
+
     //a helper function to get the restaurants of a city
-    vector<Restaurant> getRestaurantsInCityHelper(const string &city, TreeNode *node) const;
+    vector<Restaurant> getRestaurantsInCityHelper(const string& city, Restaurant* node, vector<Restaurant>& restaurantsCity) const;
     //a function to get all the restaurants in a given wilaya
-    vector<Restaurant> getRestaurantsInWilayaHelper(const string &wilaya, TreeNode *node) const;
-    
+    vector<Restaurant> getRestaurantsInWilayaHelper(const string& wilaya, Restaurant* node, vector<Restaurant>& restaurantsWilaya) const;
+
     //a function to get all the restaurants in a given district
-    vector<string>     getCitiesInDistrictHelper(const string &district) const;
-    
+    vector<string> getCitiesInDistrictHelper(const string& district) const;
+
     //helper function to get all the cuisine sales during a period of time
-    void listCuisineSalesInLocationDuringPeriod(const string &location, const string &start_date, const string &end_date,TreeNode *node) const;
-    
+    void listCuisineSalesInLocationDuringPeriod(const string& location, const string& start_date, const string& end_date, Restaurant* node) const;
+
     //helper functions for the ratio
     //function to get the ratio with location
-    void listLocationRatioHelper(TreeNode* node, const string& location, const string& start_date, const string& end_date) const ;
+    void listLocationRatioHelper(Restaurant* node, const string& location, const string& start_date, const string& end_date) const;
     //function to get the ratio by restaurant name
-     void listRestaurantRatioHelper(TreeNode* node, const string& restaurantName, const string& start_date, const string& end_date) const ;
-    
+    void listRestaurantRatioHelper(Restaurant* node, const string& restaurantName, const string& start_date, const string& end_date) const;
+
+    //function to find a prize winner
+    Restaurant* findOneCuisinePrizeWinner(int i, Restaurant* temp, Restaurant*& winner);
+
+    vector<Restaurant> getAllRestaurants(Restaurant* temp, vector<Restaurant>& restaurants);
+
+
 public:
+    //to store the 5 prize winners in a given month
+    PrizeWinners winners = PrizeWinners(2023);
+
     //constructor
-    BinarySearchTree();
+    binarySearchTree();
     //copy constructor
-    BinarySearchTree(const BinarySearchTree &rhs);
+    binarySearchTree(const binarySearchTree& rhs);
     //move constructor
-    BinarySearchTree(BinarySearchTree &&rhs);
+    binarySearchTree(binarySearchTree&& rhs);
     //destructor
-    ~BinarySearchTree();
+    ~binarySearchTree();
     //copy assignment
-    BinarySearchTree & operator=( const BinarySearchTree & rhs )
-       {
-           BinarySearchTree copy = rhs;
-           std::swap( *this, copy );
-           return *this;
-       }
+    binarySearchTree& operator=(const binarySearchTree& rhs)
+    {
+        binarySearchTree copy = rhs;
+        std::swap(*this, copy);
+        return *this;
+    }
     //move assignment
-    BinarySearchTree & operator=( BinarySearchTree && rhs )
-        {
-            std::swap( root, rhs.root );
-            return *this;
-        }
-        
+    /*binarySearchTree& operator=(binarySearchTree&& rhs)
+    {
+        std::swap(root, rhs.root);
+        return *this;
+    }*/
+
     //findMin
-    const Restaurant & findMin( ) const;
+    const Restaurant& findMin() const;
     //findMax
-    const Restaurant & findMax( ) const;
+    const Restaurant& findMax() const;
     //conatains
-    bool contains( const Restaurant & x ) const;
+    bool contains(const Restaurant& x) const;
     //tree isempty
-    bool isEmpty( ) const;
+    bool isEmpty() const;
     //printTree
-    void printTree( ostream & out = cout ) const;
+    void printTree(ostream& out = cout) const;
     //makeempty
     void makeEmpty();
-    
+
     //insert
-    void insert( const Restaurant & x );
+    void addRestaurant(const Restaurant& x);
     //delete
-    void remove( const Restaurant & x );
+    void remove(const Restaurant& x);
     //search
-    Restaurant findRestaurantByID(const string& id) const;
-    
+    Restaurant* findRestaurantByID(const string& id) const;
+    Restaurant*& findRestaurantByName(const string& name) const;
     //function to get the city code from file
-    int getCityNumberFromFile(const string &wcdFile)const;
+    int getCityNumberFromFile(const string& wcdFile)const;
     //function to get the wilaya code from file
-    int getWilayaCodeFromFile(const string &wilayaName, const string &filename)const;
-    
+    int getWilayaCodeFromFile(const string& wilayaName, const string& filename)const;
+
     //function to read the cities/wilayas/districts  from the file and initialize the vector
-    
+
     void initializeAlgerianCities(const string& filename);
     void initializeAlgerianWilayas(const string& filename);
-    vector<string> readDistrictsFromFile(const string &fileName) const;
-    
-    
+    vector<string> readDistrictsFromFile(const string& fileName) const;
+
+
     //function to get the cities of a district
     //define a struct that takes the cities and district
     struct DistrictCities
-        {
-            string district;
-            vector<string> cities;
-        };
-    vector<DistrictCities> readCitiesAndDistrictsFromFile(const string &fileName) const;
-    
-    
+    {
+        string district;
+        vector<string> cities;
+    };
+    vector<DistrictCities> readCitiesAndDistrictsFromFile(const string& fileName) const;
+
     //the getters from city/district/wilaya
-    vector<Restaurant> getRestaurantsInCity(const string &city) const;
-    vector<Restaurant> getRestaurantsInDistrict(const string &district) const;
-    vector<Restaurant> getRestaurantsInWilaya(const string &wilaya) const;
-    vector<Restaurant> getRestaurantsByLocation(const string &location) const;
-    
+    vector<Restaurant> getRestaurantsInCity(const string& city) const;
+    vector<Restaurant> getRestaurantsInDistrict(const string& district) const;
+    vector<Restaurant> getRestaurantsInWilaya(const string& wilaya) const;
+    vector<Restaurant> getRestaurantsByLocation(const string& location) const;
+
     //display the sales of each cuisine during a period of time in a location
-    void displayAllCuisineSalesInLocationDuringPeriod(const string &location, const string &start_date, const string &end_date) const ;
-    
-    void listRatioForLocation(const string& location, const string& start_date, const string& end_date) const ;
+    void displayAllCuisineSalesInLocationDuringPeriod(const string& location, const string& start_date, const string& end_date) const;
+
+    void listRatioForLocation(const string& location, const string& start_date, const string& end_date) const;
     void listRatioForRestaurant(const string& restaurantName, const string& start_date, const string& end_date) const;
 
+    //a function to get the prize winners in a given month
+    //i is the index of the cuisine 
+    //1: Algerian, 2: Chinese, 3: European, 4: Indian, 5: Syrian
+    Restaurant* findOneCuisinePrizeWinner(int i);
+    
+    vector<Restaurant> getAllRestaurants();
 };
 
 #endif
